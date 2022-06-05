89937d1e505c30aba9c9be622b2808e7a364e2d5
Andri Gunawan <andri.gnwan@gmail.com>
2022-06-05 22:35:42 +0700

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const dates = [
 
 // TODO: Buatlah fungsi createDate
 const createDate = function(dates,posisi){
-  if (posisi === undefined) {
+  if (posisi == undefined) {
     const element =[];
     for (let i = 0; i < dates.length; i++) {
          const dateMili = Date.parse(dates[i]) / 1000;
